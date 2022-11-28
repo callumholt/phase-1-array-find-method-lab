6ffadd073fd3ee34f422f598f05ee499f67dec47
callum <callumpholt@gmail.com>
2022-11-28 18:59:28 +1100

    commit tree 33da6e84b3f52ed2c4f59143bb124faba1e1b8f8

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+
+function superbowlWin(record) {
+    let winYear = record.find((record) => record.result === "W")
+    // if (winYear) {
+    //     return winYear.year
+    // }
+    // else {
+    //     return undefined;
+    // }
+
+    return  winYear ? winYear.year : undefined
+}
+
+
+
+
+// function superbowlWin(element, index, array) {
+//     return Object.keys(record).find(key => record[key] === value);
+// }
+
+// console.log(superbowlWin())
+
+
+
+
+
